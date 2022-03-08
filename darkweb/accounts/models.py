1676ac20b96d88c1484d12966633a80b30e666db
SecDet Samurai <tylerblackout17@gmail.com>
2022-03-08 22:43:23 +0300

    + setup accounts app

--- darkweb/accounts/models.py
+++ darkweb/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    """ Модель блога """
+
+    title = models.CharField(max_length=255, verbose_name="Пост")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return f"{self.title} - {self.created}"
+
+    class Meta:
+        verbose_name = "Блог"
+        verbose_name_plural = "Блоги"
--- darkweb/accounts/views.py
+++ darkweb/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+
+def index(request):
+
+    blogs = Blog.objects.all()
+
+    context = {
+        'blogs': blogs
+    }
+    return render(request, "accounts/index.html", context)
